efb222516285d0a0dd7698e82bda007f4eea6161
Kevin <kv937398@gmail.com>
2023-09-19 23:09:21 -0400

    The project

---
+++ cart.py
@@ -0,0 +1,32 @@
+from ownable import set_owner
+class Cart:
+    from item_manager import show_items
+
+    def __init__(self, owner):
+        set_owner(self, owner)
+        self.items = []
+
+    def items_list(self):
+        return self.items
+
+    def add(self, item):
+        self.items.append(item)
+
+    def total_amount(self):
+        price_list = []
+        for item in self.items:
+            price_list.append(item.price)
+        return sum(price_list)
+
+    def check_out(self):
+        if self.owner.wallet.balance < self.total_amount():
+            pass    # check_outメソッドをコーディングする際はpassは削除してください。
+        # 要件
+        #   - カートの中身（Cart#items）のすべてのアイテムの購入金額が、カートのオーナーのウォレットからアイテムのオーナーのウォレットに移されること。
+        #   - カートの中身（Cart#items）のすべてのアイテムのオーナー権限が、カートのオーナーに移されること。
+        #   - カートの中身（Cart#items）が空になること。
+        # ヒント
+        #   - カートのオーナーのウォレット ==> self.owner.wallet
+        #   - アイテムのオーナーのウォレット ==> item.owner.wallet
+        #   - お金が移されるということ ==> (？)のウォレットからその分を引き出して、(？)のウォレットにその分を入金するということ
+        #   - アイテムのオーナー権限がカートのオーナーに移されること ==> オーナーの書き換え（item.owner = ?）
---
+++ item.py
@@ -0,0 +1,19 @@
+from ownable import set_owner
+
+class Item:
+    instances = []
+
+    def __init__(self, name, price, owner=None):
+        self.name = name
+        self.price = price
+        set_owner(self, owner)
+        # Itemインスタンスの生成時、そのItemインスタンス(self)は、insntancesというクラス変数に格納されます。
+        Item.instances.append(self)
+
+    def label(self):
+        return {"name": self.name, "price": self.price}
+
+    @staticmethod
+    def item_all():
+        # instancesを返します ==> Item.item_all()でこれまでに生成されたItemインスタンスを全て返すということです。
+        return Item.instances
